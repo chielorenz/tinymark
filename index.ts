ad5ab4f2a55ffb4a374b681dbc4a414e93504e57
Luca Lorenzini <chielorenz@gmail.com>
2022-08-05 00:19:47 +0200

    Update index use new lexer

--- index.ts
+++ index.ts
@@ -1,22 +1,12 @@
-// import { feed } from "./ts/lexer.js";
-// import { useParser } from "./ts/parser.js";
-// import { make } from "./ts/maker.js";
-
-// const buff = `# Hello, world!`;
-// This is a paragraph.`;
-// const lexer = feed(buff);
-// const parse = useParser(lexer);
-// const html = make(parse);
-
-// console.log(html);
-
 import useLexer from "./ts/lexer.js";
 import useParser from "./ts/parser.js";
 import { make } from "./ts/maker.js";
 
-const input = `# Hello, world!`;
+const input = `# Hello, world!
+This is a paragraph.
+# Hello, world!`;
 const lexer = useLexer(input);
 const parser = useParser(lexer);
 const html = make(parser);
 
-console.log(html);
+console.log("Result:", html);
