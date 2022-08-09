2dbcd14cb09abde31eddac195273452d32e07fe7
Luca Lorenzini <chielorenz@gmail.com>
2022-08-09 14:21:23 +0200

    Color debug messages

--- ts/utils.ts
+++ ts/utils.ts
@@ -1,14 +1,18 @@
+const BLUE = "\x1b[34m";
+const RESET = "\x1b[0m";
+
 export const useLog =
 	(label: string) =>
 	(...msg: any[]) => {
-		if (process.env.DEBUG === "true") console.log("Debug |", label, ...msg);
+		if (process.env.DEBUG === "true")
+			console.log(BLUE, label, ...msg, RESET);
 	};
 
 export const useLogDeep =
 	(label: string) =>
 	(object: any, ...msg: any[]) => {
 		if (process.env.DEBUG === "true") {
-			console.log("Debug |", label, ...msg);
+			console.log(BLUE, label, ...msg, RESET);
 			console.dir(object, { depth: null });
 		}
 	};
